c15e43a51a52c2b29e5650be5cccb2d3443bf154
vashenkoda <d.vaschenko@haulmont.net>
2023-11-29 09:39:34 +0300

    Day 1 finished

--- src/main/java/com/company/jmixpm/entity/Project.java
+++ src/main/java/com/company/jmixpm/entity/Project.java
@@ -1,12 +1,15 @@
 package com.company.jmixpm.entity;
 
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.Composition;
 import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @JmixEntity
@@ -35,6 +38,43 @@ public class Project {
     @JoinColumn(name = "MANAGER_ID", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private User manager;
+
+    @Column(name = "STATUS")
+    private Integer status;
+
+    @JoinTable(name = "PROJECT_USER_LINK",
+            joinColumns = @JoinColumn(name = "PROJECT_ID", referencedColumnName = "ID"),
+            inverseJoinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"))
+    @ManyToMany
+    private List<User> participants;
+
+    @Composition
+    @OneToMany(mappedBy = "project")
+    private List<Task> tasks;
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+
+    public ProjectStatus getStatus() {
+        return status == null ? null : ProjectStatus.fromId(status);
+    }
+
+    public void setStatus(ProjectStatus status) {
+        this.status = status == null ? null : status.getId();
+    }
 
     public User getManager() {
         return manager;
@@ -74,5 +114,12 @@ public class Project {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+
+        setStartDate(LocalDateTime.now());
+
     }
 }
--- src/main/java/com/company/jmixpm/entity/Task.java
+++ src/main/java/com/company/jmixpm/entity/Task.java
@@ -1,6 +1,8 @@
 package com.company.jmixpm.entity;
 
+import io.jmix.core.DeletePolicy;
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.OnDeleteInverse;
 import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
 
@@ -10,7 +12,9 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @JmixEntity
-@Table(name = "TASK_")
+@Table(name = "TASK_", indexes = {
+        @Index(name = "IDX_TASK__PROJECT", columnList = "PROJECT_ID")
+})
 @Entity(name = "Task_")
 public class Task {
     @JmixGeneratedValue
@@ -33,6 +37,18 @@ public class Task {
 
     @Column(name = "ESTIMATED_EFFORTS")
     private Integer estimatedEfforts;
+    @OnDeleteInverse(DeletePolicy.CASCADE)
+    @JoinColumn(name = "PROJECT_ID", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Project project;
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 
     public Integer getEstimatedEfforts() {
         return estimatedEfforts;
